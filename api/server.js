56f88cfe757b06b08299645679c15e606ec957b2
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-04-15 15:08:03 -0300

    feat: update server

--- api/server.js
+++ api/server.js
@@ -4,20 +4,29 @@ const path = require("path");
 
 const server = jsonServer.create();
 
-const filePath = path.join("db.json");
-const data = fs.readFileSync(filePath, "utf-8");
-const db = JSON.parse(data);
-const router = jsonServer.router(db);
-
+const filePath = path.join(__dirname, "db.json");
 const middlewares = jsonServer.defaults();
-
 server.use(middlewares);
+
 server.use(
   jsonServer.rewriter({
     "/api/*": "/$1",
   })
 );
+
+server.delete("/tasks", (req, res) => {
+  const dbData = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+
+  dbData.tasks = [];
+
+  fs.writeFileSync(filePath, JSON.stringify(dbData, null, 2), "utf-8");
+
+  res.status(204).send();
+});
+
+const router = jsonServer.router(filePath);
 server.use(router);
+
 server.listen(3000, () => {
   console.log("JSON Server is running");
 });
